dc66ca25dc9ddb7d3df61f8a586ca5357bfe65ff
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-02 02:11:46 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,4 @@
+console.error("HALP!")
 const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
@@ -20,7 +21,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log("HALP!")', done => {
+  it('calls console.log()', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
